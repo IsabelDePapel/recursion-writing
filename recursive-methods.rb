d1e0d5be7a9d40bf06f34d8198896f0d9811ae43
IsabelDePapel <isuchanek@gmail.com>
2017-11-18 15:46:57 -0800

    completed

--- recursive-methods.rb
+++ recursive-methods.rb
@@ -1,34 +1,128 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
+
+# time: O(n) because it needs to execute n times to calculate factorial
+# space: O(n) because of the stack space (n calls to factorial)
 def factorial(n)
-  puts "Not implemented."
+  if n == 0 || n == 1
+    return 1
+  else
+    return n * (factorial n - 1)
+  end
 end
 
-def reverse(s)
-  puts "Not implemented."
+# time: O(n) because it needs to execute n times to reverse the string
+# space: O(n) because there are n calls on the stack (O(n)) plus O(n) for creating the new string, which is O(2n), which simplifies to O(n)
+def reverse(s, reversed = "", idx = s.length - 1)
+  len = s.length
+
+  if len == 0 || len == 1
+    return s
+  # if end of string
+  elsif idx < 0
+    return reversed
+  else
+    reversed << s[idx]
+    return reverse(s, reversed, idx - 1)
+  end
 end
 
-def reverse_inplace(s)
-  puts "Not implemented."
+# time: O(n) because it makes n/2 calls to the function (which simplifies to O(n))
+# space: O(n) because the reversal is in place, but there are n/2 calls on the stack, which simplifies to O(n)
+def reverse_inplace(s, start_idx = 0, end_idx = s.length - 1)
+  len = (end_idx - start_idx + 1)
+
+  if len == 0 || len == 1
+    return s
+  else
+    # swap outer chars
+    temp = s[start_idx]
+    s[start_idx] = s[end_idx]
+    s[end_idx] = temp
+
+    return reverse_inplace(s, start_idx + 1, end_idx - 1)
+  end
 end
 
+# time: O(n) because the function gets called n times
+# space: O(n) because there are n calls to the function on the stack
 def bunny(n)
-  puts "Not implemented."
+  # n is num bunnies; each bunny has 2 ears; compute num ears
+  if n == 0
+    return 0
+  else
+    return 2 + bunny(n - 1)
+  end
 end
 
-def nested(s)
-  puts "Not implemented."
+# time: O(n) because it makes n/2 calls to the function, which simplifies to O(n)
+# space: O(n) for the same reason--n/2 calls on the stack, which simplifies to O(n)
+def nested(s, start_idx = 0, end_idx = s.length - 1)
+  # assumes empty string returns true
+  len = (end_idx - start_idx + 1)
+
+  if len.odd?
+    return false
+  elsif len == 0
+    return true
+  else
+    if s[start_idx] != "(" || s[end_idx] != ")"
+      return false
+    else
+      return nested(s, start_idx + 1, end_idx - 1)
+    end
+  end
 end
 
-def search(array, value)
-  puts "Not implemented."
+# time: O(n) because it has to call the function n times to search the entire array
+# space: O(n) because there will be n calls to the function on the stack to search the entire array
+def search(array, value, idx = 0)
+  # array is UNSORTED
+  len = array.length
+
+  return false if len == 0
+
+  # if found
+  if array[idx] == value
+    return true
+  # if end of array
+  elsif idx + 1 == len
+    return false
+  # search rest of array
+  else
+    return search(array, value, idx + 1)
+  end
 end
 
-def is_palindrome(s)
-  puts "Not implemented."
+# time: O(n) because it calls the function n/2 times, which simplifies to O(n)
+# space: O(n) because there are n/2 function calls on the stack, which simplifies to O(n)
+def is_palindrome(s, start_idx = 0, end_idx = s.length - 1)
+  # assumes empty string is a palindrome
+  len = (end_idx - start_idx + 1)
+
+  if len == 0 || len == 1
+    return true
+  else
+    if s[start_idx] != s[end_idx]
+      return false
+    else
+      return is_palindrome(s, start_idx + 1, end_idx - 1)
+    end
+  end
 end
 
-def digit_match(n, m)
-  puts "Not implemented."
+# time: O(log m) where m is the smaller of the two integers. If there are k digits in m, it will make k calls to the function, and k is equal to log10(m) + 1
+# space: O(log m) because there will be log m calls on the stack (and any additional memory used isn't dependent on input size)
+def digit_match(n, m, matches = 0)
+  if n == 0 || m == 0
+    return matches
+  else
+    # check ones digit
+    if n % 10 == m % 10
+      matches += 1
+    end
+
+    return digit_match(n / 10, m / 10, matches)
+  end
 end
 
 # Factorial Tests
